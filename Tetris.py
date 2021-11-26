80f5fab8a60bf4b4020df58856608b073d8ca640
ylei532 <ylei532@aucklanduni.ac.nz>
2021-11-26 15:09:41 +1300

    user input implemented for tetris game

--- Tetris.py
+++ Tetris.py
@@ -18,7 +18,7 @@ pieces = [
          ]
 
 
-
+lock = threading.Lock()
 
 class TetrisGame():
     
@@ -26,13 +26,111 @@ class TetrisGame():
         self.grid = [["_" for i in range(MAXWIDTH)] for i in range(MAXHEIGHT)]
         self.loci = []
         self.game_over = False
+   
+    def CheckGame(self):
+        count = 0
+        for i in range(4, MAXHEIGHT):
+            for j in range(MAXWIDTH):
+                if self.grid[i][j] == "\033[4m0\033[0m":
+                    count+=1
+                else:
+                    break
+            # if count == 10:
+                
+            #     for k in range(MAXWIDTH):
+            #         self.grid[i][k] = "_"
+                    
+                
+                
+                
+                
+                
+                        
+            # count = 0
     
     def UserInput(self):
+        '''
+        Handles user inputs
+        '''
+        no_space = False
         while True:
-            x = input("hi")
-            break
+            if self.game_over == True:
+                break
+            x = input("")
+            # move left
+            if x.lower() == 'a':
+                lock.acquire()
+                array = np.array(self.loci)
+                if min(array[:,1]) >= 1:
+                    
+                    for x, y in self.loci:
+                        self.grid[x][y] = "_"
+                        
+                    new_loci = [[x, y-1] for x, y in array[:,:]]
+                    
+                    for x, y in new_loci:
+                        if self.grid[x][y] != "_":
+                            no_space = True
+                            break
+                   
+                    if no_space:
+                        for x, y in self.loci:
+                            self.grid[x][y] = "\033[4m0\033[0m"
+                        no_space = False
+                        lock.release()
+                        continue
+                    
+                    for x, y in self.loci:
+                        self.grid[x][y] = "_"
+                        
+                    self.loci = new_loci
+               
+                    for x, y in self.loci:
+                        self.grid[x][y] = "\033[4m0\033[0m"
+                    self.display_grid()
+                lock.release()
+            
+            # move right
+            elif x.lower() == 'd':
+                lock.acquire()
+                array = np.array(self.loci)
+                if max(array[:,1]) <= 8:
+                    
+                    for x, y in self.loci:
+                        self.grid[x][y] = "_"
+                        
+                    new_loci = [[x, y+1] for x, y in array[:,:]]
+                    
+                    for x, y in new_loci:
+                        if self.grid[x][y] != "_":
+                            no_space = True
+                            break
+                   
+                    if no_space:
+                        for x, y in self.loci:
+                            self.grid[x][y] = "\033[4m0\033[0m"
+                        no_space = False
+                        lock.release()
+                        continue
+                    
+                    for x, y in self.loci:
+                        self.grid[x][y] = "_"
+                        
+                    self.loci = new_loci
+               
+                    for x, y in self.loci:
+                        self.grid[x][y] = "\033[4m0\033[0m"
+                    self.display_grid()
+                lock.release()
+            
+            elif x.lower() == 'f':
+                self.game_over = True                
     
     def display_grid(self):
+        '''
+        Show the current status of the grid
+        '''
+        
         for i in range(MAXWIDTH):
             print(" "+"_", end="")
         print('')
@@ -43,13 +141,20 @@ class TetrisGame():
             print('|')
 
     def reset_grid(self):
+        '''
+        Clears all items out of the grid by resetting instance variables
+        '''
         self.grid = [["_" for i in range(MAXWIDTH)] for i in range(MAXHEIGHT)]
         self.loci = []
         self.game_over = False        
 
     def insert_piece(self):
+        '''
+        Handles the insertion of a piece into the tetris grid
+        '''
+        
         self.loci.clear()
-        for x, y in pieces[1]:
+        for x, y in pieces[random.randint(0, 6)]:
             while self.grid[x+4][y+3] != "_":
                 x-=1
             if x+4 == 1:
@@ -61,8 +166,10 @@ class TetrisGame():
             self.loci.append([x+4,y+3])
             self.grid[x+4][y+3] = "\033[4m0\033[0m"
 
-    @staticmethod
     def StartGame(self):
+        '''
+        Hangles tetris game logic
+        '''
         
         self.reset_grid()
         self.insert_piece()
@@ -73,14 +180,26 @@ class TetrisGame():
             next_piece = False
             
             while True:
-                time.sleep(0.5)
+                
+                if self.game_over == True: # check if user ends game
+                    break
+                
+                self.display_grid()
+                time.sleep(0.25)
                 numpy_loci = np.array(self.loci)
                 if max(numpy_loci[:,0]) == 23:
                     break
                 
+                if self.game_over == True: # check if user ends game
+                    break
+                
+                
                 for x,y in self.loci:
                     self.grid[x][y] = "_"
-                    
+                
+                if self.game_over == True: # check if user ends game
+                    break    
+                
                 for x,y in self.loci:
                     if self.grid[x+1][y] != "_":                                            
                        for x,y in self.loci:
@@ -88,36 +207,42 @@ class TetrisGame():
                        next_piece = True
                        break
                 
+                if self.game_over == True: # check if user ends game
+                    break
+                
                 if next_piece:
                     next_piece = False
                     break
-                        
+                
                 for i in range(len(self.loci)):
                     self.grid[self.loci[i][0]+1][self.loci[i][1]] = "\033[4m0\033[0m"
                     self.loci[i][0]+=1
                 
                 self.display_grid()
-
-          
+ 
+            if self.game_over == True: # check if user ends game
+                break
+            
             self.insert_piece()
             self.display_grid()
-            time.sleep(0.5)
-         
+            
+    
+            time.sleep(0.25)    
           
 def main():
 
     game = TetrisGame()
     
-    thread = threading.Thread(target=TetrisGame.UserInput, args=[game])
+    thread1 = threading.Thread(target=TetrisGame.UserInput, args=[game])
     thread2 = threading.Thread(target=TetrisGame.StartGame, args=[game])
-    thread.start()
+    thread1.start()
     thread2.start()
     
     # game = TetrisGame()
     # game.start()
     
     # game.join()
-    thread.join()
+    thread1.join()
     thread2.join()
             
     # game = TetrisGame()
